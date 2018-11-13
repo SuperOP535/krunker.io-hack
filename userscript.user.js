9ea01975a1487366acc557b92e74eb89684f5683
Faber <37834714+xF4b3r@users.noreply.github.com>
2018-11-13 22:03:38 +0100

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -5,7 +5,7 @@
 // @downloadURL  https://github.com/xF4b3r/krunker/raw/master/userscript.user.js
 // @version      2.14
 // @author       Faber, collaborators: William Thomson, Tehchy
-// @include      /^https?:\/\/krunker\.io(|\/|\/\?server=\d+\.\d+\.\d+)$/
+// @include      /^https?:\/\/krunker\.io(|\/|\/\?server=.+)$/
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
 // ==/UserScript==
